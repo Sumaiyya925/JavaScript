b42dfd4d076f193e8209e8c91d758f9b524e51aa
Sumaiyya Farhat <sumaiyyafarhat925@gmail.com>
2024-05-09 16:42:57 +0500

    JAVASCRIPT

---
+++ index.js
@@ -0,0 +1,37 @@
+// let person ={
+// name:'sumaiyya',
+// age:19
+// };
+// person.name='mosh'
+// console.log(person.name);
+
+// let courseNames = ['html','CSS','JavaScript'];
+// //I can aslo change the values of the array dynamically
+// courseNames[3] = 1;
+// console.log(courseNames[3]);
+
+// //FUNCTIONS
+
+// //PERFORMING A TASK FUNCTION
+// function pookie(name,lastname) {
+//     console.log('hello '+ name + ' ' +lastname);
+// }
+
+// pookie('Aiza','pookie');
+
+// //CALCULATING FUNCTION
+// function calculator(number){
+//    return number*number;
+// }
+
+// let result = calculator(43);
+// console.log('The square of 43 is: ');
+// console.log(result);
+
+//TERNARY OPERATOR
+
+console.log('TERNARY OPERATOR');
+let customerPoints = 80;
+let typeOfCustomer = customerPoints>100 ? 'GOLD CUSTOMER' : 'SILVER CUSTOMER';
+console.log('The type of the customer is: ');
+console.log(typeOfCustomer);

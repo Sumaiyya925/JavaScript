31db2b1fc46488dd976534ed174f1813abc12dda
Sumaiyya Farhat <sumaiyyafarhat925@gmail.com>
2024-05-09 20:11:03 +0500

    LOOPS

--- index.js
+++ index.js
@@ -61,16 +61,76 @@
 
 //if/else
 
-let hour=20;
+// let hour=20;
 
-if(hour>=6 && hour<12){
-    console.log('GOOD MORNING!');
-}
+// if(hour>=6 && hour<12){
+//     console.log('GOOD MORNING!');
+// }
 
-else if(hour>=12 && hour<18){
-    console.log("GOOD AFTERNOON!");
-}
+// else if(hour>=12 && hour<18){
+//     console.log("GOOD AFTERNOON!");
+// }
 
-else{
-    console.log("GOOD EVENING!");
+// else{
+//     console.log("GOOD EVENING!");
+// }
+
+//SWITCH
+
+// let role = "moderator";
+
+// switch(role){
+//     case"guest":
+//         console.log("GUEST USER");
+//         break;
+//     case "moderator":
+//         console.log("MODERATOR USER");
+//         break;
+
+//         case "admin":
+//             console.log("ADMIN USER");
+//             break;
+
+//             default:
+//             console.log("UNKNOWN USER!");
+
+// }
+
+//LOOPS
+
+//for loop
+
+// for(let i=1; i<=6; i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+//while loop
+
+// let i = 1;
+// while(i<=6){
+//     if(i%2==0) console.log(i);
+//     i++;
+// }
+
+
+//for-in loop
+
+// let person={
+// name:'mosh',
+// age:30
+// };
+
+// for(let key in person){
+//     console.log(key,person[key]);
+// }
+
+
+//using for-in loop for arrays
+
+const colors=['red','purple','green'];
+
+for(let index in colors){
+    console.log(index,colors[index]);
 }

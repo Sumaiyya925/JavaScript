7ac95686f595113301c6404a89e21df615d1a049
Sumaiyya Farhat <sumaiyyafarhat925@gmail.com>
2024-05-31 00:07:09 +0500

    STAIRCASE

--- staircase.js
+++ staircase.js
@@ -1,13 +1,17 @@
 function staircase(n) {
-    //for spaces
-    for(let i=0; i<n; i++){
-        for(let j=0; j<n; j++){
-            console.log(' ');
-        }
+  for (let i = 1; i <= n; i++) {
+    //row
+    let str = "";
 
-        console.log('#');
+    for (let j = n - 1; j >= i; j--) {
+      str += " ";
+    }
+    for (let k = 1; k <= i; k++) {
+      str += "#";
     }
 
+    console.log(str);
+  }
 }
 
-staircase(3);
+staircase(5);

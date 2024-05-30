b49926812345fbef5bebd125171f19e76f548c4a
Sumaiyya Farhat <sumaiyyafarhat925@gmail.com>
2024-05-30 20:32:47 +0500

    PLUS MINUS RATIO

---
+++ plusMinus.js
@@ -0,0 +1,34 @@
+function plusMinus(arr) {
+  let posCount = 0;
+  let negCount = 0;
+  let zeroCount = 0;
+  let posRatio = 0;
+  let negRatio = 0;
+  let zeroRatio = 0;
+
+  for(let key of arr){
+    if(key > 0){
+        posCount++;
+    }
+    if(key < 0){
+        negCount++;
+    }
+    if( key === 0 ){
+        zeroCount++;
+    }
+    
+  }
+
+  posRatio = posCount / arr.length;
+  negRatio = negCount / arr.length;
+  zeroRatio = zeroCount / arr.length;
+
+  console.log(posRatio.toFixed(6));
+  console.log(negRatio.toFixed(6));
+  console.log(zeroRatio.toFixed(6));
+
+}
+
+let numbers = [-4,3,-9,0,4,1];
+
+plusMinus(numbers);

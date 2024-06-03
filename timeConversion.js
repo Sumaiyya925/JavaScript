d6a2658bee2c78f3613e35e37e1ee1beebaf992b
Sumaiyya Farhat <sumaiyyafarhat925@gmail.com>
2024-06-03 20:40:22 +0500

    TIME CONVERSION

--- timeConversion.js
+++ timeConversion.js
@@ -1,30 +1,3 @@
-// let s = "07:05:45PM";
-// s = s.replace("PM", "");
-// let a = s.split(":");
-// console.log(a);
-// a[0] = Number(a[0]);
-// a[0] += 12;
-// let newArr = a.join(":");
-// console.log(newArr);
-
-// let s = "12:05:45AM";
-// s = s.replace("AM", "");
-// // let a = s.split(":");
-// s = s.replace("12", "00");
-// console.log(s);
-
-//HOW TO FIND THE AM AND PM
-
-// let str = "07:05:45PM";
-// let a = str.split("");
-// console.log(a);
-// if (a.includes("A")) {
-//   console.log("AM");
-// } else {
-//   console.log("PM");
-// }
-// console.log(a.join(""));
-
 let str = "12:00:00AM";
 console.log(timeConversion(str));
 function timeConversion(s) {

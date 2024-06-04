9ea2f9c674a9762bbcb462dfe40378042b726612
Sumaiyya Farhat <sumaiyyafarhat925@gmail.com>
2024-06-04 22:11:00 +0500

    PAIRS

--- pair.js
+++ pair.js
@@ -1,19 +1,11 @@
 a = [1, 2, 3, 4];
-// k = 1;
-// let count = 0;
-// for (let i = 0; i < a.length; i++) {
-//   for (let j = i + 1; j < a.length; j++) {
-//     if (Math.abs(i - j) === k) {
-//       count++;
-//     }
-//   }
-// }
-// console.log(count);
+
 console.log(pairs(1, a));
 function pairs(k, arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++)
     for (let j = i + 1; j < arr.length; j++)
       if (Math.abs(arr[i] - arr[j]) === k) count++;
+
   return count;
 }
